03622bc5fea9eb1ae45b58b9709b491c1b4d21da
armandoluja <lujasaa@rose-hulman.edu>
2016-01-18 01:45:08 -0500

    Added create story and write story activity
    I also made it possible to navigate to those activities, with the
    appropriate actions on back button pressed, cancel etc..

--- StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/storieswithfriends/CreateStoryActivity.java
+++ StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/storieswithfriends/CreateStoryActivity.java
@@ -1,6 +1,8 @@
 package edu.rosehulman.lujasaa.storieswithfriends;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,21 +11,45 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class SettingsActivity extends AppCompatActivity {
+public class CreateStoryActivity extends AppCompatActivity {
+    private Button mCreateBtn;
+    private Button mCancelBtn;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        setContentView(R.layout.activity_create_story);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        mContext = getBaseContext();
+        mCreateBtn = (Button)findViewById(R.id.create_story_button);
+        mCancelBtn = (Button)findViewById(R.id.cancel_create_button);
+
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent writeStory = new Intent(mContext, WriteStoryActivity.class);
+                startActivity(writeStory);
+            }
+        });
+
+        mCancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     @Override
@@ -36,4 +62,5 @@ public class SettingsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
--- StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/storieswithfriends/WriteStoryActivity.java
+++ StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/storieswithfriends/WriteStoryActivity.java
@@ -14,12 +14,12 @@ import android.support.v4.app.NavUtils;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class SettingsActivity extends AppCompatActivity {
-
+public class WriteStoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        setContentView(R.layout.activity_write_story);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -35,5 +35,10 @@ public class SettingsActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpFromSameTask(this);
     }
 }
