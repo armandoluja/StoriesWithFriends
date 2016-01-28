f9a87c5337cd26bcfd1fc01e18c009e117b776d2
Kice Sanders <KiceSanders@gmail.com>
2016-01-27 21:41:23 -0500

    Friend adapter correctly showing user friends

--- StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Adapters/FriendAdapter.java
+++ StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Adapters/FriendAdapter.java
@@ -1,37 +1,51 @@
 package edu.rosehulman.lujasaa.swf.Adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 import edu.rosehulman.lujasaa.swf.R;
 import edu.rosehulman.lujasaa.swf.User;
+
 
 /**
  * Created by sanderkd on 1/23/2016.
  */
 public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder> {
-    private ArrayList<User> friendArray;
+    private ArrayList<Friend> friendArray;
+    private Firebase mFriendRef;
+    private String FRIEND_PATH = "https://lujasaa-stories-with-friends.firebaseio.com/friend/sanderkd@rose-hulman%edu";
 
-    public FriendAdapter(){
+    public FriendAdapter() {
         friendArray = new ArrayList<>();
+        mFriendRef = new Firebase(FRIEND_PATH);
+        mFriendRef.addChildEventListener(new FriendChildListener());
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_create_story_friend_view, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.friend_row, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mText.setText(friendArray.get(position).getUsername());
-        holder.mImage.setImageBitmap(friendArray.get(position).getImage());
+        Log.d("db", "friend array 0: " +friendArray.get(0).displayName);
+        holder.mText.setText(friendArray.get(position).displayName);
+//        holder.mImage.setImageBitmap(friendArray.get(position).icon);
     }
 
     @Override
@@ -42,10 +56,64 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView mImage;
         private TextView mText;
+
         public ViewHolder(View itemView) {
             super(itemView);
             mImage = (ImageView) itemView.findViewById(R.id.friend_row_image);
             mText = (TextView) itemView.findViewById(R.id.friend_row_text);
+        }
+    }
+
+    private class FriendChildListener implements ChildEventListener {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            final String friendID = (String) dataSnapshot.getValue();
+            Log.d("db", "friendID: " + friendID);
+            final Firebase displayName = new Firebase("https://lujasaa-stories-with-friends.firebaseio.com/user/" + friendID + "/displayName");
+            displayName.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Log.d("db", "displayName: " + dataSnapshot.getValue());
+                    Friend f = new Friend(friendID, (String) dataSnapshot.getValue());
+                    Log.d("db", "friend id: "+ f.friendID + " displayName: " + f.displayName);
+                    friendArray.add(f);
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            //shouldn't happen
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            //shouldn't happen
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            //shouldn't happen
+        }
+
+        @Override
+        public void onCancelled(FirebaseError firebaseError) {
+            //shouldn't happen
+        }
+    }
+
+    private class Friend{
+        public String friendID;
+        public String displayName;
+        Friend(String friendID, String  displayName){
+            this.friendID = friendID;
+            this.displayName = displayName;
         }
     }
 }
--- StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Fragments/FriendsFragment.java
+++ StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Fragments/FriendsFragment.java
@@ -3,10 +3,13 @@ package edu.rosehulman.lujasaa.swf.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.rosehulman.lujasaa.swf.Adapters.FriendAdapter;
 import edu.rosehulman.lujasaa.swf.R;
 
 /**
@@ -23,6 +26,11 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.friend_recycler_view);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rv.hasFixedSize();
+        rv.setAdapter(new FriendAdapter());
+        return view;
     }
 }
