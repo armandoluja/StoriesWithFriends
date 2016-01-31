6d9b49c72db8ef798f2ec8c5bf78646491436d5e
armandoluja <lujasaa@rose-hulman.edu>
2016-01-31 17:45:08 -0500

    moved the fab to the create story fragment

--- StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Activities/MainActivity.java
+++ StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Activities/MainActivity.java
@@ -86,14 +86,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent createStory = new Intent(view.getContext(), CreateStoryActivity.class);
-                startActivity(createStory);
-            }
-        });
+
         mFragmentManager = getSupportFragmentManager();
     }
 
--- StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Fragments/MyCurrentStoriesFragment.java
+++ StoriesWithFriends/app/src/main/java/edu/rosehulman/lujasaa/swf/Fragments/MyCurrentStoriesFragment.java
@@ -1,12 +1,15 @@
 package edu.rosehulman.lujasaa.swf.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.rosehulman.lujasaa.swf.Activities.CreateStoryActivity;
 import edu.rosehulman.lujasaa.swf.R;
 
 /**
@@ -27,7 +30,16 @@ public class MyCurrentStoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_current_stories, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_current_stories, container, false);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createStory = new Intent(view.getContext(), CreateStoryActivity.class);
+                startActivity(createStory);
+            }
+        });
+        return view;
     }
 
 }
